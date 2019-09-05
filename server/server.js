9af2424c60cbc1d8bbdc18482a490e3003380abc
Tate Stone <tatejamesstone@gmail.com>
2019-09-05 12:01:06 -0600

    More work

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const ctrl = require("./controller/controller")
+
+const app = express()
+
+app.use(express.json())
+
+const baseUrl = "/api/messages"
+app.post(baseUrl, ctrl.create)
+app.get(baseUrl, ctrl.read)
+app.put(`${baseUrl}/:id`, ctrl.update)
+app.delete(`${baseUrl}/:id`, ctrl.delete)
+
+let port = 3001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
